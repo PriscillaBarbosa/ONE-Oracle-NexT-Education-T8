2e537199f5bce52150e39391c0c557955ae9b5a1
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-01-16 20:36:40 -0300

    desafio-js-projeto_inicial

---
+++ desafio-js-projeto_inicial/app.js
@@ -0,0 +1,18 @@
+alert ("Boas vindas ao nosso site!");
+
+var nome = "lua";
+var idade = 25;
+var numeroDeVendas = 50;
+var saldoDisponível = 1000;
+
+alert ("Erro! Preencha todos os campos");
+
+var mensagemDeErro = "Erro! Preencha todos os campos.";
+alert (mensagemDeErro);
+
+var nome = prompt ("Qual é o seu nome? .");
+var idade = prompt ("Qual é a sua idade? .");
+if (idade >= 18) {
+    alert ("Pode tirar a habilitação!");
+}
+
