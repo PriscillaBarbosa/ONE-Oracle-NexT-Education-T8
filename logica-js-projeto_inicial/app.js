73010cd5e1672e609da480b9330530f456df738c
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-01-22 20:05:23 -0300

    complementando o exercicio de lógica

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,24 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
-}
+//enquanto chute não for igual a 5
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 30');
+
+    //se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o ${numeroSecreto} com ${tentativas} tentativa(s)`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}.`);
+        } else {
+            alert(`O número secreto é maior que ${chute}. `);
+        }
+        //tentativas = tentativas + 1
+        tentativas ++;
+    }
+} 
