5bec1e879ad1b73f99e0b889a6c613db4b2378ca
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-01-25 18:38:29 -0300

    finalizando o programa calculo do peso ideal

--- livro-logica-js/programa-calculo-peso-ideal/js/index.js
+++ livro-logica-js/programa-calculo-peso-ideal/js/index.js
@@ -2,8 +2,22 @@
 const frm = document.querySelector('form');
 const resp = document.querySelector('h3');
 
-//cria um "ouvinte" de evento, acionado quando o botão submit for clicado
-frm.addEventListener('submit', (e) => {
-    const nome = frm.inNome.value
-    resp.innerText = `Olá ${nome}`
-    e.preventDefault})
+//"ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();  //evita que o envio do form
+
+    const nome = frm.inNome.value;   //obtém valores do form
+    const masculino = frm.inMasculino.checked;
+    const altura = Number(frm.inAltura.value);
+
+    const peso = masculino ? 22 * Math.pow(altura, 2) : 21* Math.pow(altura, 2);
+    
+    
+    //apresenta a resposta (altera o conteúdo do elemento h3 da página)
+    resp.innerHTML = `${nome}: Seu peso ideal é ${peso.toFixed(3)} kg`;
+
+    frm.addEventListener("reset", () => {
+        resp.innerText = "" //limpa o conteúdo do elemento h3 que exibe a resposta
+    });
+
+});
