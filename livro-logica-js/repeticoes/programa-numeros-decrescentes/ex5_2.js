b648a5cb42c9b7cf0449d7a863d97a088e130a51
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-01-30 22:47:55 -0300

    programa números decrescentes

---
+++ livro-logica-js/repeticoes/programa-numeros-decrescentes/ex5_2.js
@@ -0,0 +1,12 @@
+const frm = document.querySelector("form") //obtém elementos da página
+const resp = document.querySelector("h3") 
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault() //evita envio do form
+    const numero = Number(frm.inNumero.value) //obtém número informado
+    let resposta = `Entre ${numero} e 1: `  //string para montar a resposta
+    for (let i = numero; i > 0; i = i - 1) { //cria um for decrescente
+        resposta = resposta + i + ", "  //resposta acumula números + vírgula
+    }
+    resp.innerText = resposta
+})
